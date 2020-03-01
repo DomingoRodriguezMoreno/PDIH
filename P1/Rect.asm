122887bbc873e04186fab890d6c25ee4217800b2
Domingo Rodríguez <dominrodri5@gmail.com>
2020-03-01 10:31:29 +0100

    Practica 1

---
+++ P1/Rect.asm
@@ -0,0 +1,90 @@
+TEXTO   EQU 3
+GRAFICO EQU 4
+BLANCO  EQU 3
+ROSA    EQU 2
+VERDE   EQU 1
+
+;MACRO pausa_tecla
+;espera la pulsacion de una tecla
+pausa_tecla MACRO
+        push ax
+        mov ah,0   ;funcion para leer una tecla
+        int 16h    ;interrupcion BIOS para teclado
+        pop ax
+ENDM
+
+;MACRO modo_video
+;cambia el modo de video (texto-3 o grafico-4)
+modo_video MACRO modo
+        push ax
+        mov al,modo
+        mov ah,0
+        int 10h
+        pop ax
+ENDM
+
+;MACRO pixel
+;pone un pixel en la coordenada X,Y de color C
+pixel MACRO X,Y,C
+        push ax
+        push cx
+		push dx
+		mov ax,Y
+		mov cx,X
+		mov dx,ax
+        mov al,C
+        mov ah,0Ch
+        int 10h
+        pop dx
+        pop cx
+		pop ax
+ENDM
+
+pila segment stack 'stack'
+	dw 100h dup (?)
+pila ends
+
+datos segment 'data'
+		msg_pulsa_tecla db 'pulsa para continuar...',7,'$'
+datos ends
+
+
+codigo segment 'code'
+	assume cs:codigo, ds:datos, ss:pila
+	main PROC
+		mov ax,datos
+		mov ds,ax
+
+		mov dx,OFFSET msg_pulsa_tecla
+		mov ah,9
+		int 21h
+		pausa_tecla
+
+		modo_video GRAFICO
+
+		;pintar una linea horizontal
+		mov cx,200
+		mov bx,60
+
+		bucle2:
+			mov cx,200
+		bucle1:
+			
+			pixel cx,bx,BLANCO
+			dec cx
+			cmp cx,100
+			jne bucle1
+
+			dec bx
+			cmp bx, 20
+			jne bucle2
+
+		pausa_tecla
+		modo_video TEXTO
+
+		mov ax,4C00h
+		int 21h
+	main ENDP
+codigo ends
+
+END main
---
+++ P1/Video.asm
@@ -0,0 +1,41 @@
+pila segment stack 'stack'
+	dw 100h dup (?)
+pila ends
+
+datos segment 'data'
+	msg db 'Cambio de modo realizado $'
+datos ends
+
+codigo segment 'code'
+	assume cs:codigo, ds:datos, ss:pila
+	main PROC
+		mov ax,datos
+		mov ds,ax
+
+		; poner modo de video 40x25
+		mov al,0 ; modo de 40x25
+		mov ah,0 ; funcion de poner modo de video
+		int 10h
+
+		; escribir una cadena
+		mov dx,OFFSET msg
+		mov ah,9
+		int 21h
+
+		; hacer una pausa esperando una tecla
+		mov ah,08h
+		int 21h
+
+		; poner modo de video 80x25
+		mov al,3 ; modo de 80x25
+		mov ah,0 ; funcion de poner modo de video
+		int 10h
+
+        ; terminar y salir
+        mov ax, 4C00h
+		int 21h
+
+	main ENDP
+codigo ends
+
+END main
